645357e220b8ff68849272f3e43618d2b2035629
liaoyz999999 <46314483+liaoyz999999@users.noreply.github.com>
2021-06-11 16:33:23 +0800

    测试提交2

---
+++ src/main/java/com/timing/qywx/demo/test/B.java
@@ -0,0 +1,11 @@
+package com.timing.qywx.demo.test;
+
+/**
+ * @author liaoyz
+ * @version v1.0
+ * @description: 类说明 （必填）
+ * @date 2021/06/11 16:33
+ * @change 2021/06/11 16:33 liaoyz@v1.0 创建
+ */
+public class B {
+}
