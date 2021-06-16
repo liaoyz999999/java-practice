edcd343b2af2bc382d7734dd137b605eb39cac49
liaoyz999999 <46314483+liaoyz999999@users.noreply.github.com>
2021-06-16 10:18:28 +0800

    分模块后基础配置提交

---
+++ file/src/test/java/com/lyz/file/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyz.file;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FileApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
